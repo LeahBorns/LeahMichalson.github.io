188249017410d1d9885c13d633759d380382ee19
5e34d362a834270cb09987e4 <leah.borns@gmail.com>
2020-08-13 10:57:53 -0700

    Removed mailTo javascript

--- script.js
+++ script.js
@@ -53,12 +53,4 @@ $(window).on('load resize scroll', function () {
     });
 });
 
-// The following example POSTS data to mailthis.to, redirects the user to a confirmation page, and then sends an email (upon the successful completion of Recaptcha verification)
 
-$.post('https://mailthis.to/test@example.com', {
-    email: 'foo@bar.co',
-    _subject: 'hi!',
-    message: 'Test'
-}).then(function () {
-    location.href = 'https://mailthis.to/confirm'
-});
