b952c2fe767e88d4fe9a1b41a610b127ae8b6c45
5e34d362a834270cb09987e4 <leah.borns@gmail.com>
2020-08-14 12:02:47 -0700

    edited home screen, trying to get nav to stick

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 'use strict';
 
-//$(document).ready(function () {
-
-
-//});
-
-
 
 var mobileBtn = document.querySelector('.mobile-btn');
 var menu = document.querySelector('ul#nav-links');
@@ -38,7 +32,7 @@ window.addEventListener('click', function (event) {
     }
 });
 
-//sideways scrolling effect
+//sideways scrolling effect on home screen
 
 $(window).on('load resize scroll', function () {
     $('.bg-static').each(function () {
@@ -53,42 +47,3 @@ $(window).on('load resize scroll', function () {
     });
 });
 
-(function ($) {
-    'use strict';
-
-    var form = $('.contact__form'),
-        message = $('.contact__msg'),
-        form_data;
-
-    // Success function
-    function done_func(response) {
-        message.fadeIn()
-        message.html(response);
-        setTimeout(function () {
-            message.fadeOut();
-        }, 5000);
-
-        form.find('input:not([type="submit"]), textarea').val('');
-    }
-
-    // fail function
-    function fail_func(data) {
-        message.fadeIn()
-        message.html(data.responseText);
-        setTimeout(function () {
-            message.fadeOut(5000);
-        }, 5000);
-    }
-
-    form.submit(function (e) {
-        e.preventDefault();
-        form_data = $(this).serialize();
-        $.ajax({
-            type: 'POST',
-            url: form.attr('action'),
-            data: form_data
-        })
-            .done(done_func)
-            .fail(fail_func);
-    });
-})(jQuery);
