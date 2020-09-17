873aa762f1acc3653b31450a649f7266240288e7
5e34d362a834270cb09987e4 <leah.borns@gmail.com>
2020-09-17 12:39:05 -0700

    updated formspree form

--- js/connect-form.js
+++ js/connect-form.js
@@ -1,39 +1,88 @@
-(function ($) {
-    'use strict';
+//(function ($) {
+//    'use strict';
+//
+//    var form = $('.contact__form'),
+//        message = $('.contact__msg'),
+//        form_data;
+//
+//    // Success function
+//    function done_func(response) {
+//        message.fadeIn()
+//        message.html(response);
+//        setTimeout(function () {
+//            message.fadeOut();
+//        }, 5000);
+//
+//        form.find('input:not([type="submit"]), textarea').val('');
+//    }
+//
+//    // fail function
+//    function fail_func(data) {
+//        message.fadeIn()
+//        message.html(data.responseText);
+//        setTimeout(function () {
+//            message.fadeOut(5000);
+//        }, 5000);
+//    }
+//
+//    form.submit(function (e) {
+//        e.preventDefault();
+//        form_data = $(this).serialize();
+//        $(this).trigger("reset");
+//        console.log("I'm reset!")
+//        $.ajax({
+//            type: 'POST',
+//            url: form.attr('action'),
+//            data: form_data
+//        })
+//            .done(done_func)
+//            .fail(fail_func);
+//    });
+//})(jQuery);
 
-    var form = $('.contact__form'),
-        message = $('.contact__msg'),
-        form_data;
 
-    // Success function
-    function done_func(response) {
-        message.fadeIn()
-        message.html(response);
-        setTimeout(function () {
-            message.fadeOut();
-        }, 5000);
+window.addEventListener("DOMContentLoaded", function() {
 
-        form.find('input:not([type="submit"]), textarea').val('');
+    // get the form elements defined in your form HTML above
+
+    var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+
+    function success() {
+        form.reset();
+        button.style = "display: none ";
+        status.innerHTML = "Thanks! Your message was sent";
     }
 
-    // fail function
-    function fail_func(data) {
-        message.fadeIn()
-        message.html(data.responseText);
-        setTimeout(function () {
-            message.fadeOut(5000);
-        }, 5000);
+    function error() {
+        status.innerHTML = "Oops! There was a problem.";
     }
 
-    form.submit(function (e) {
-        e.preventDefault();
-        form_data = $(this).serialize();
-        $.ajax({
-            type: 'POST',
-            url: form.attr('action'),
-            data: form_data
-        })
-            .done(done_func)
-            .fail(fail_func);
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
     });
-})(jQuery);
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
